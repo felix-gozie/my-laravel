64a17af8b561bcf5b1a3398dc87dfa64250983cf
felix-gozie <felixchigozie272@gmail.com>
2024-12-12 11:53:59 -0800

    discription for productController

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -1,19 +1,18 @@
 <?php
-
+// This code was imported to avoid naming conflict
 namespace App\Http\Controllers;
+//this code is imported for a user to be able to perform actions on the users table like adding new users to the users table and retrieving data
 use App\Models\Product;
 
-
-use Illuminate\Http\Request;
-
+//what this code does is that it inherit laravel funtionalities from the base controller which handles HTTP request
 class productController extends Controller
 {
     public function show($id)
 {
-    // Find the product by ID or return a 404 error if not found
+    //retrieves the product record by id, throws 404 not found error if product is not found
     $product = Product::findOrFail($id);
     
-    // Pass the product data to the 'nav_describe' view
+    //it returns to product page if the product exist 
     return view('product', compact('product'));
 }
 
